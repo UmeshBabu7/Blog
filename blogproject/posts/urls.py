6880ea867de3778757c77891c11d67d50d79f19e
Umesh <umeshtamang129@gmail.com>
2024-06-28 16:12:50 +0545

    setup templates and using extends tag

--- blogproject/posts/urls.py
+++ blogproject/posts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    
+    path('',views.index,name='posts_home'),
+    path('about/',views.about,name='posts_about'),
+    path('services/',views.services,name='posts_services')
 ]
--- blogproject/posts/views.py
+++ blogproject/posts/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    posts=[
+        {
+            "id":1,
+            "title":"Python",
+            "author":"Umesh Tamang"
+        },
+         {
+            "id":2,
+            "title":"Django",
+            "author":"Ramesh Tamang"
+        },
+         {
+            "id":3,
+            "title":"Mysql",
+            "author":"Babu Tamang"
+        }
+    ]
+
+    context={
+        'posts':posts,
+        'title':'Home Page '
+    }
+
+    return render(request,'home.html',context)
+
+def about(request):
+    context={
+        "title":"About Page "
+    }
+    return render(request,'about.html',context)
+
+def services(request):
+    context={
+        "title":"Services Page "
+    }
+    return render(request,'services.html',context)
