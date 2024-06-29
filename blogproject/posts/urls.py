7e44e6cd118946c27f816f98963e7c30bbd1b38f
Umesh <umeshtamang129@gmail.com>
2024-06-29 09:16:20 +0545

    using forms for create posts

--- blogproject/posts/urls.py
+++ blogproject/posts/urls.py
@@ -3,5 +3,6 @@ from .import views
 urlpatterns = [
     path('',views.index,name='posts_home'),
     path('about/',views.about,name='posts_about'),
-    path('services/',views.services,name='posts_services')
+    path('services/',views.services,name='posts_services'),
+    path('create_post/',views.create_post,name='create_post')
 ]
--- blogproject/posts/views.py
+++ blogproject/posts/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import PostCreationForm
+from .models import Post
 
 # Create your views here.
 def index(request):
-    posts=[
-        {
-            "id":1,
-            "title":"Python",
-            "author":"Umesh Tamang"
-        },
-         {
-            "id":2,
-            "title":"Django",
-            "author":"Ramesh Tamang"
-        },
-         {
-            "id":3,
-            "title":"Mysql",
-            "author":"Babu Tamang"
-        }
-    ]
+    # posts=[
+    #     {
+    #         "id":1,
+    #         "title":"Python",
+    #         "author":"Umesh Tamang"
+    #     },
+    #      {
+    #         "id":2,
+    #         "title":"Django",
+    #         "author":"Ramesh Tamang"
+    #     },
+    #      {
+    #         "id":3,
+    #         "title":"Mysql",
+    #         "author":"Babu Tamang"
+    #     }
+    # ]
+    posts=Post.objects.all()
 
     context={
         'posts':posts,
@@ -38,3 +41,22 @@ def services(request):
         "title":"Services Page "
     }
     return render(request,'services.html',context)
+
+def create_post(request):
+    form=PostCreationForm()
+
+    if request.method =="POST":
+        data=request.POST
+
+        title=data["title"]
+        content=data["content"]
+        author=data["author"]
+
+        new_post=Post(
+            title=title,content=content,author=author)
+        new_post.save()
+
+        return redirect('posts_home')
+    context={'form':form}
+    return render(request,'createpost.html',context)
+
