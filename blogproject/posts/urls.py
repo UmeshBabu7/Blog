caf975283f8bbc141ab17377a47c7430ee70a770
Umesh <umeshtamang129@gmail.com>
2024-10-15 12:33:02 +0545

    create all posts and one posts for views and urls mapping

--- blogproject/posts/urls.py
+++ blogproject/posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.index,name="index_home")
+    path('',views.index,name="posts_home"),
+    path('posts/',views.return_all_posts,name="all_posts"),
+    path('post/<int:post_id>/',views.return_one_post,name="one_post")
 ]
--- blogproject/posts/views.py
+++ blogproject/posts/views.py
@@ -3,5 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts=[
+    {
+        "id":1,
+        "title":"Python",
+        "author":"Umesh"
+    },
+    {
+        "id":2,
+        "title":"Javascript",
+        "author":"Ramesh"
+    },
+    {
+        "id":3,
+        "title":"Dotnet",
+        "author":"Ishan"
+    },
+]
+
 def index(request):
     return HttpResponse("Hello umesh")
+
+
+def return_all_posts(request):
+    return HttpResponse(str(posts))
+
+def return_one_post(request,post_id):
+    for post in posts:
+        if post["id"] ==post_id:
+            return HttpResponse(str(post))
+        
+    return HttpResponse("Not found")
