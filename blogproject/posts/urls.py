5d68df3ccccd9fc297429f254acf07d1199ca6d1
Umesh <umeshtamang129@gmail.com>
2024-10-17 12:51:17 +0545

    Model Forms

--- blogproject/posts/urls.py
+++ blogproject/posts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.index,name="posts_home"),
     path('about/',views.about,name="posts_about"),
     path('services/',views.services,name="posts_services"),
-    path('create_post/',views.create_post,name="create_post")
+    path('create_post/',views.create_post,name="create_post"),
+    path('post/<int:post_id>',views.post_detail,name='post_detail'),
+    path('post/update/<int:post_id>/',views.update_post,name='update_post')
 ]
