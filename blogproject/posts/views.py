29b4f8102abb997e7faf7e2ddf807129e1055ad7
Umesh <umeshtamang129@gmail.com>
2024-10-19 09:17:54 +0545

    Using Class Based Views

--- blogproject/posts/views.py
+++ blogproject/posts/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import PostCreationForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
 
 
-def index(request):
-    posts=Post.objects.all()
-    context={
+# def index(request):
+#     posts=Post.objects.all()
+#     context={
+#         'posts':posts
+#     }
+#     return render(request,'index.html',context)
+
+class HomePageView(View):
+    template_name="index.html"
+
+    def get(self,request):
+        posts=Post.objects.all()
+        context={
         'posts':posts
     }
-    return render(request,'index.html',context)
+        return render(request,self.template_name,context)
 
 
 def about(request):
@@ -28,22 +41,22 @@ def services(request):
     }
     return render(request,'services.html',context)
 
+@method_decorator(login_required,'dispatch')
+class CreatePostView(View):
+    template_name='createpost.html'
+    form_class=PostCreationForm
+    initial_values={"key":"value"}
 
-def create_post(request):
-    form=PostCreationForm()
+    def get(self,request):
+        form=self.form_class(initial=self.initial_values)
+        context={
+            "form":form
+        }
+        return render(request,'createpost.html',context)
+    def post(self,request):
+        form=self.form_class(request.POST,request.FILES)
 
-    if request.method =="POST":
-        form = PostCreationForm(request.POST , request.FILES)
 
-        if form.is_valid():
-            form.save()
-            return redirect('posts_home')
-   
-    context={
-        'form':form
-
-    }
-    return render(request,'createpost.html',context)
 
 
 def post_detail(request,post_id):
@@ -53,6 +66,7 @@ def post_detail(request,post_id):
     return render(request,'post_detail.html',context)
 
 
+@login_required
 def update_post(request,post_id):
     post_to_update=Post.objects.get(pk=post_id)
     form=PostCreationForm(instance=post_to_update)
